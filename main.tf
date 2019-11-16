18f8211411cb4e3a95ae079be7f5c93d37094190
Toby Clemson <tobyclemson@gmail.com>
2019-11-16 23:25:32 +0000

    Fix resource name.

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_iam_role" "role" {
   assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
 }
 
-resource "aws_iam_role_policy_attachment" "admin" {
+resource "aws_iam_role_policy_attachment" "role" {
   role = aws_iam_role.role.name
   policy_arn = var.policy_arn
 }
